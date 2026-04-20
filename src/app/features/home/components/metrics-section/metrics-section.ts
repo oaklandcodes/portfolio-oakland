f6b8bd095c44b3544dd7401fd38eadb24223d5a1
Oakland <oakland.caro91@gmail.com>
2026-04-20 15:05:41 +0200

    feat: enhance metrics and projects sections with improved layout and styling

--- src/app/features/home/components/metrics-section/metrics-section.ts
+++ src/app/features/home/components/metrics-section/metrics-section.ts
@@ -1,12 +1,23 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MetricItem } from '../../data/portfolio.models';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-metrics-section',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule], // Solo necesitamos CommonModule para el @for
   templateUrl: './metrics-section.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MetricsSection {
   readonly metrics = input.required<MetricItem[]>();
+
+  getIconType(label: string): string {
+  const l = label.toLowerCase();
+  if (l.includes('año') || l.includes('experiencia')) return 'history';
+  if (l.includes('proyecto')) return 'rocket';
+  if (l.includes('certifica') || l.includes('grado')) return 'certificate';
+  if (l.includes('ingl') || l.includes('idioma')) return 'language';
+  return 'star';
+}
 }
